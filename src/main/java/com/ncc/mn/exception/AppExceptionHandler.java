f96e37122138649234829cbecde5ae4550a21627
manh nguyen <manhnguyen16112@gmail.com>
2022-05-20 17:35:16 +0700

    test change

--- src/main/java/com/ncc/mn/exception/AppExceptionHandler.java
+++ src/main/java/com/ncc/mn/exception/AppExceptionHandler.java
@@ -4,7 +4,6 @@ package com.ncc.mn.exception;
 import com.ncc.mn.model.ExceptionResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -21,11 +20,11 @@ public class AppExceptionHandler {
         return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    @ExceptionHandler(BadCredentialsException.class)
-    public ResponseEntity<ExceptionResponse> handleLogin(BadCredentialsException ex, WebRequest req){
-        ExceptionResponse response = ExceptionResponse.builder().timestamp(new Date()).message(ex.getMessage()).build();
-        return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
-    }
+//    @ExceptionHandler(BadCredentialsException.class)
+//    public ResponseEntity<ExceptionResponse> handleLogin(BadCredentialsException ex, WebRequest req){
+//        ExceptionResponse response = ExceptionResponse.builder().timestamp(new Date()).message(ex.getMessage()).build();
+//        return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
+//    }
 
 
     //handle all exception
--- src/main/java/com/ncc/mn/service/login/CustomUserDetailService.java
+++ src/main/java/com/ncc/mn/service/login/CustomUserDetailService.java
@@ -1,22 +1,22 @@
-package com.ncc.mn.service.login;
-
-import com.ncc.mn.entity.User;
-import com.ncc.mn.model.CustomUserDetail;
-import com.ncc.mn.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-
-@Service
-public class CustomUserDetailService implements UserDetailsService {
-    @Autowired
-    private UserRepository userRepository;
-    @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(email);
-        if(user == null) throw new UsernameNotFoundException(email);
-        return new CustomUserDetail(user);
-    }
-}
+//package com.ncc.mn.service.login;
+//
+//import com.ncc.mn.entity.User;
+//import com.ncc.mn.model.CustomUserDetail;
+//import com.ncc.mn.repository.UserRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class CustomUserDetailService implements UserDetailsService {
+//    @Autowired
+//    private UserRepository userRepository;
+//    @Override
+//    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+//        User user = userRepository.findByEmail(email);
+//        if(user == null) throw new UsernameNotFoundException(email);
+//        return new CustomUserDetail(user);
+//    }
+//}
