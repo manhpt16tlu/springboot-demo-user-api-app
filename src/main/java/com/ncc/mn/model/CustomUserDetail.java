c48a0573f5bccb26240878e6f682cae5d512af9a
manh nguyen <manhnguyen16112@gmail.com>
2022-05-20 21:50:21 +0700

    fix some issue

--- src/main/java/com/ncc/mn/model/CustomUserDetail.java
+++ src/main/java/com/ncc/mn/model/CustomUserDetail.java
@@ -1,50 +1,50 @@
-package com.ncc.mn.model;
-
-import com.ncc.mn.entity.User;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.ArrayList;
-import java.util.Collection;
-
-@NoArgsConstructor
-@AllArgsConstructor
-public class CustomUserDetail implements UserDetails {
-    private User user;
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return new ArrayList<>();
-    }
-
-    @Override
-    public String getPassword() {
-        return user.getEncpyptPassword();
-    }
-
-    @Override
-    public String getUsername() {
-        return user.getEmail();
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
-}
+//package com.ncc.mn.model;
+//
+//import com.ncc.mn.entity.User;
+//import lombok.AllArgsConstructor;
+//import lombok.NoArgsConstructor;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.userdetails.UserDetails;
+//
+//import java.util.ArrayList;
+//import java.util.Collection;
+//
+//@NoArgsConstructor
+//@AllArgsConstructor
+//public class CustomUserDetail implements UserDetails {
+//    private User user;
+//    @Override
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        return new ArrayList<>();
+//    }
+//
+//    @Override
+//    public String getPassword() {
+//        return user.getEncpyptPassword();
+//    }
+//
+//    @Override
+//    public String getUsername() {
+//        return user.getEmail();
+//    }
+//
+//    @Override
+//    public boolean isAccountNonExpired() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isAccountNonLocked() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isCredentialsNonExpired() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isEnabled() {
+//        return true;
+//    }
+//}
